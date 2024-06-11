c115b1c882406f268fab7eb322aa6eb365d87904
Sakshi-Greycube <sakshi@greycube.in>
2024-06-11 16:03:26 +0530

    fix: solve error in employee salary calculation

--- humam/api.py
+++ humam/api.py
@@ -2,4 +2,4 @@ import frappe
 from frappe.model.document import Document
 
 def calculate_total_salary_of_employee(self,method):
-    self.custom_total_salary = self.custom_basic_salary + self.custom_transportation_cost + self.custom_housing_cost
+    self.custom_total_salary = (self.custom_basic_salary or 0) + (self.custom_transportation_cost or 0) + (self.custom_housing_cost or 0)
